8ecf8547925d57064938a59ccc110081d53c8477
Murray Steele <contact@murto.dev>
2019-09-10 18:01:53 +0100

    Spawn a ManageTODOs widget by default

--- medoit/medoit.py
+++ medoit/medoit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+from widgets import ManageTODOs
 import sys
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
-  main = QWidget()
+  main = ManageTODOs()
   main.show()
   sys.exit(app.exec_())
