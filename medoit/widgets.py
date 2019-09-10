2e3cd71c74d37aef9c0346b3d54bb600ff6db7a9
Murray Steele <contact@murto.dev>
2019-09-10 18:01:29 +0100

    Add stub ManageTODOs widget

--- medoit/widgets.py
+++ medoit/widgets.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python
+
+from PyQt5.QtWidgets import QBoxLayout, QLabel, QListWidget, QPushButton, QWidget
+from PyQt5.QtCore import Qt
+
+
+class ManageTODOs(QWidget):
+
+  def __init__(self):
+    super().__init__()
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
+    self.todos = QListWidget()
+    self.new = QPushButton('New')
+    self.view = QPushButton('View')
+    self.rename = QPushButton('Rename')
+    self.delete = QPushButton('Delete')
+    layout.addWidget(self.todos, alignment=Qt.AlignCenter)
+    layout.addWidget(self.new, alignment=Qt.AlignCenter)
+    layout.addWidget(self.view, alignment=Qt.AlignCenter)
+    layout.addWidget(self.rename, alignment=Qt.AlignCenter)
+    layout.addWidget(self.delete, alignment=Qt.AlignCenter)
+    self.setLayout(layout)
+
+  def setNewCallback(self, callback):
+    self.new.clicked.connect(callback)
+
+  def setViewCallback(self, callback):
+    self.view.clicked.connect(callback)
+
+  def setRenameCallback(self, callback):
+    self.rename.clicked.connect(callback)
+
+  def setDeleteCallback(self, callback):
+    self.delete.clicked.connect(callback)
