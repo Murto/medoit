18992361aae703757c8084264cceb684d8005109
Murray Steele <contact@murto.dev>
2019-09-10 17:22:09 +0100

    Create QWidget stub

--- medoit/medoit.py
+++ medoit/medoit.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
 if __name__ == '__main__':
-  pass
+  app = QApplication(sys.argv)
+  main = QWidget()
+  main.show()
+  sys.exit(app.exec_())
