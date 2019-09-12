429f6ca69b9825baa6072fe475441ddbb611c8b0
Murray Steele <contact@murto.dev>
2019-09-12 16:33:44 +0100

    Make it possible to add a new TODO list

--- medoit/medoit.py
+++ medoit/medoit.py
@@ -8,17 +8,20 @@ import sys
 
 def createNewTODODialog(manager):
   dialog = NewTODODialog()
-  def createNewTODO(name):
-    manager.add(TODO(name))
-    dialog.close()
-  dialog.setOKCallback(createNewTODO)
+  def okCallback(name):
+    if not any(name == todo.name for todo in manager.todos):
+      manager.add(TODO(name))
+      dialog.close()
+  dialog.setOKCallback(okCallback)
   dialog.setCancelCallback(dialog.close)
   dialog.exec_()
 
 if __name__ == '__main__':
   manager = TODOManager()
   app = QApplication(sys.argv)
-  main = ManageTODOs()
+  main = ManageTODOs(manager)
+  def newTODODialogCallback():
+    createNewTODODialog(manager)
   main.setNewCallback(lambda: createNewTODODialog(manager))
   main.show()
   sys.exit(app.exec_())
