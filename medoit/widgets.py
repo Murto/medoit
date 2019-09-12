b6f801ae0f61c8555a8bebd8086706d5e519362a
Murray Steele <contact@murto.dev>
2019-09-12 17:30:29 +0100

    Make widgets independent of operations concerning the backend

--- medoit/widgets.py
+++ medoit/widgets.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python
 
-from PyQt5.QtWidgets import QBoxLayout, QDialog, QLabel, QLineEdit, QListWidget, QPushButton, QWidget
+from PyQt5.QtWidgets import QBoxLayout, QDialog, QLabel, QLineEdit, QListWidget, QPushButton, QStackedWidget, QWidget
 from PyQt5.QtCore import Qt
 
 
 class ManageTODOs(QWidget):
 
-  def __init__(self, manager):
+  def __init__(self):
     super().__init__()
-    self.manager = manager
-    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
     self.todos = QListWidget()
     self.new = QPushButton('New')
     self.view = QPushButton('View')
     self.rename = QPushButton('Rename')
     self.delete = QPushButton('Delete')
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
     layout.addWidget(self.todos, alignment=Qt.AlignCenter)
     layout.addWidget(self.new, alignment=Qt.AlignCenter)
     layout.addWidget(self.view, alignment=Qt.AlignCenter)
     layout.addWidget(self.rename, alignment=Qt.AlignCenter)
     layout.addWidget(self.delete, alignment=Qt.AlignCenter)
     self.setLayout(layout)
-    self.manager.attach(self.update)
-    self.update()
 
-  def update(self):
+  def update(self, manager):
     self.todos.clear()
-    print([todo.name for todo in self.manager.todos])
-    self.todos.addItems([todo.name for todo in self.manager.todos])
+    self.todos.addItems([todo.name for todo in manager.todos])
 
   def setNewCallback(self, callback):
     self.new.clicked.connect(callback)
@@ -46,11 +42,11 @@ class NewTODODialog(QDialog):
 
   def __init__(self):
     super().__init__()
-    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
     self.instructions = QLabel('Enter a name for your new TODO list:')
     self.name = QLineEdit()
     self.ok = QPushButton('OK')
     self.cancel = QPushButton('Cancel')
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
     layout.addWidget(self.instructions, alignment=Qt.AlignCenter)
     layout.addWidget(self.name, alignment=Qt.AlignCenter)
     layout.addWidget(self.ok, alignment=Qt.AlignCenter)
@@ -62,3 +58,36 @@ class NewTODODialog(QDialog):
 
   def setCancelCallback(self, callback):
     self.cancel.clicked.connect(callback)
+
+
+class ViewTODO(QWidget):
+
+  def __init__(self):
+    super().__init__()
+    self.manager = manager
+    self.todo = todo
+    self.title = QLabel(self.todo.name)
+    self.items = QListWidget()
+    self.add = QPushButton('Add')
+    self.remove = QPushButton('Remove')
+    self.back = QPushButton('Back')
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
+    layout.addWidget(self.title, alignment=Qt.AlignCenter)
+    layout.addWidget(self.items, alignment=Qt.AlignCenter)
+    layout.addWidget(self.add, alignment=Qt.AlignCenter)
+    layout.addWidget(self.remove, alignment=Qt.AlignCenter)
+    layout.addWidget(self.back, alignment=Qt.AlignCenter)
+    self.setLayout(layout)
+
+  def update(self, todo):
+    self.items.clear()
+    self.items.addItems([item.description for item in todo.items])
+
+  def setAddCallback(self, callback):
+    self.add.clicked.connect(callback)
+
+  def setRemoveCallback(self, callback):
+    self.remove.clicked.connect(lambda: callback(self.items.currentItem()))
+  
+  def setBackCallback(self, callback):
+    self.back.clicked.connect(callback)
