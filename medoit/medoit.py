2c473bc4f6b684af4127e9813f284ca35aa2caf3
Murray Steele <contact@murto.dev>
2019-09-12 10:32:55 +0100

    Add calls to 'New TODO' dialog

--- medoit/medoit.py
+++ medoit/medoit.py
@@ -2,11 +2,23 @@
 
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtCore import Qt
-from widgets import ManageTODOs
+from todo import TODOManager, TODO
+from widgets import ManageTODOs, NewTODODialog
 import sys
 
+def createNewTODODialog(manager):
+  dialog = NewTODODialog()
+  def createNewTODO(name):
+    manager.add(TODO(name))
+    dialog.close()
+  dialog.setOKCallback(createNewTODO)
+  dialog.setCancelCallback(dialog.close)
+  dialog.exec_()
+
 if __name__ == '__main__':
+  manager = TODOManager()
   app = QApplication(sys.argv)
   main = ManageTODOs()
+  main.setNewCallback(lambda: createNewTODODialog(manager))
   main.show()
   sys.exit(app.exec_())
